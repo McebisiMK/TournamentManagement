98623c4c70baacd65f21c0549b19529b1e336190
mcebisim <mcebisim@sahomeloans.com>
2019-11-11 12:27:55 +0200

    Removed unnecessary using

--- TournamentManagement.Repositories/Team/TeamRepository.cs
+++ TournamentManagement.Repositories/Team/TeamRepository.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Text;
 using System.Threading.Tasks;
 using TournamentManagement.Entities.Models;
 using TournamentManagement.IRepository;
--- TournamentManagement.Repositories/Tournament/TournamentRepository.cs
+++ TournamentManagement.Repositories/Tournament/TournamentRepository.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TournamentManagement.Entities.Models;
