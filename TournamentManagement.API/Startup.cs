323447d558d3d32ba8dab835999ee9000f8a867f
mcebisim <mcebisim@sahomeloans.com>
2019-11-11 12:26:39 +0200

    Removed unused connection string

--- TournamentManagement.API/Startup.cs
+++ TournamentManagement.API/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using TournamentManagement.Entities.Models;
 using TournamentManagement.IRepository;
 using TournamentManagement.IRepository_Registration;
