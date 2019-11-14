9253d2ecbc784c695ad81bd3714b93b2f4d0acd4
mcebisim <mcebisim@sahomeloans.com>
2019-11-14 15:06:34 +0200

    Added tournament model reference

--- TournamentManagement.Web/src/app/shared/services/tournament.service.ts
+++ TournamentManagement.Web/src/app/shared/services/tournament.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Tournament } from '../models/tournament.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TournamentService {
+  tournamentData: Tournament;
 
   constructor() { }
 }
