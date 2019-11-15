42c866e7234aa95db9353fe5efd4bcf655a73ddb
mcebisim <mcebisim@sahomeloans.com>
2019-11-15 14:27:32 +0200

    Added post and reset form methods

--- TournamentManagement.Web/src/app/tournaments/register/register.component.ts
+++ TournamentManagement.Web/src/app/tournaments/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { TournamentService } from 'src/app/shared/services/tournament.service';
 
 @Component({
   selector: 'app-register',
@@ -7,8 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: TournamentService) { }
 
   ngOnInit() {
+    this.resetForm();
+  }
+  
+    onSubmit(form: NgForm) {
+      this.service.save(form.value)
+        .subscribe(
+          response => { 
+            this.resetForm(form);
+          },
+          error => {
+            console.log(error);
+           }
+          )
+    }
+
+  resetForm(form?: NgForm) {
+    if (form != null)
+      form.resetForm();
+    this.service.tournament = {
+      Id: null,
+      Name: '',
+      Location: '',
+      StartDate: null
+    }
   }
 }
