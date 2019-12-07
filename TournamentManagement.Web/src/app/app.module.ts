37980730ad210e693663a2f8b9d4e31b5f7c82a5
mcebisi <mcebisimkhohliwe@gmail.com>
2019-12-07 15:22:13 +0200

    Added route for registration

--- TournamentManagement.Web/src/app/app.module.ts
+++ TournamentManagement.Web/src/app/app.module.ts
@@ -20,11 +20,15 @@ import { TeamListComponent } from "./teams/team-list/team-list.component";
 import { TournamentsComponent } from "./tournaments/tournaments.component";
 import { RegisterTournamentComponent } from "./tournaments/register-tournament/register-tournament.component";
 import { TournamentListComponent } from "./tournaments/tournament-list/tournament-list.component";
+import { RegistrationComponent } from './registration/registration.component';
+import { RegisterComponent } from './registration/register/register.component';
+import { RegistrationListComponent } from './registration/registration-list/registration-list.component';
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "tournaments", pathMatch: "full" },
   { path: "tournaments", component: TournamentsComponent },
-  { path: "teams", component: TeamsComponent }
+  { path: "teams", component: TeamsComponent },
+  { path: "registration", component: RegistrationComponent }
 ];
 @NgModule({
   declarations: [
@@ -35,7 +39,10 @@ const appRoutes: Routes = [
     TeamListComponent,
     TournamentsComponent,
     RegisterTournamentComponent,
-    TournamentListComponent
+    TournamentListComponent,
+    RegistrationComponent,
+    RegisterComponent,
+    RegistrationListComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
