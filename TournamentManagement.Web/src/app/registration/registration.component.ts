3a1ce4953ee6933cc13544af7667bd5c0396e0ab
mcebisi <mcebisimkhohliwe@gmail.com>
2019-12-07 15:18:18 +0200

    Added registration component

---
+++ TournamentManagement.Web/src/app/registration/registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styles: []
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
