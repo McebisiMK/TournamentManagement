0f7b839a0a2f6ce4c5e106aca2c592be10fc0e69
mcebisi <mcebisimkhohliwe@gmail.com>
2019-12-08 23:47:09 +0200

    Added 'Delete','Update' and 'Save' methods

--- TournamentManagement.Web/src/app/shared/services/registration.service.ts
+++ TournamentManagement.Web/src/app/shared/services/registration.service.ts
@@ -1,11 +1,25 @@
-import { Injectable } from '@angular/core';
-import { Registration } from '../models/registration.model';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Registration } from "../models/registration.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class RegistrationService {
   registration: Registration;
+  private readonly rootUrl = "http://localhost:52991/api/registration";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  save(registration: Registration) {
+    return this.http.post(this.rootUrl + "/save", registration);
+  }
+
+  update(id: Number, registration: Registration) {
+    return this.http.put(this.rootUrl + "/update/" + id, registration);
+  }
+
+  delete(id: Number) {
+    return this.http.delete(this.rootUrl + "/delete/" + id);
+  }
 }
